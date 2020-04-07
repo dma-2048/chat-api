4cdeed242e64cd68c88c20c6261e9389e44c20da
dma-2048 <54641605+dma-2048@users.noreply.github.com>
2020-04-06 23:33:24 -0400

    Add files via upload

---
+++ src/demo-loadbalancing/cloud-app-info.js
@@ -0,0 +1,7 @@
+var AppInfo = {
+    //	Wss: true,
+    AppId: "82778c77-fa6f-4d3e-8dd0-86ea75d22654",
+    AppVersion: "1.0",
+    //	MasterServer: "localhost:9090"
+    //    FbAppId: "you fb app id", 
+}
